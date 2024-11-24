0f33e7ab87eede6b7486741f801dce8167f26700
KwiatekMiki <hi@kwiatekmiki.pl>
2024-11-24 22:21:41 +0100

    feat: soundcloud support
    closes #4

--- src/routes/getvideos/+server.ts
+++ src/routes/getvideos/+server.ts
@@ -1,5 +1,9 @@
 import { Client } from "youtubei";
+import { Soundcloud } from "soundcloud.ts";
+
 const youtube = new Client();
+const soundcloud = new Soundcloud();
+
 const errorMessages = {
     noLink: "you forgot about the link!",
     invalidLink: "that doesn't look like a valid link, are you sure it's the right one?",
@@ -17,6 +21,15 @@ async function getYouTubeVideos(playlistId: string): Promise<Array<string> | Res
     }
 }
 
+async function getSoundcloudTracks(playlistUrl: string): Promise<Array<string> | Response> {
+    const playlist = await soundcloud.playlists.get(playlistUrl);
+    if (playlist) {
+        return playlist.tracks.map(track => track.permalink_url)
+    } else {
+        return new Response(errorMessages.invalidLink, { status: 400 });
+    }
+}
+
 export async function GET({ url }) {
     try {
         const playlistUrl = await url.searchParams.get("url") || "";
@@ -25,10 +38,13 @@ export async function GET({ url }) {
         }
 
         const youtubeMatch = playlistUrl?.match(/PL[A-Za-z0-9_-]+/);
+        const soundcloudMatch = playlistUrl?.match(/soundcloud\.com\/[a-z0-9-_]+\/sets\/[a-z0-9-_]+/);
         let playlistVideos;
 
         if (youtubeMatch) {
             playlistVideos = await getYouTubeVideos(youtubeMatch[0]);
+        } else if (soundcloudMatch) {
+            playlistVideos = await getSoundcloudTracks(playlistUrl);
         } else {
             return new Response(errorMessages.invalidLink, { status: 400 });
         }
